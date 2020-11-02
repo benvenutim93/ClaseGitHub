977e95a3e795b94bf64059bd1adae654827ba5a0
benvenutim93 <56692322+benvenutim93@users.noreply.github.com>
2020-11-02 17:26:32 -0300

    Eliminacion de funcion suma

--- pruebaGit/main.c
+++ pruebaGit/main.c
@@ -2,11 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-int suma (int a, int b)
-{
-    return a+b;
-}
-
 int main()
 {
     printf("Hello world!\n");
@@ -17,7 +12,7 @@ int main()
 	printf("Probando el uso de ramas desde GitBash\n");
 
 
-	printf("%d", suma(1,2));
+
 
 
     return 0;
