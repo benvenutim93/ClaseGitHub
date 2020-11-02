a82bcf8e72e81d3d9d787ac6a0f076712a35d807
benvenutim93 <56692322+benvenutim93@users.noreply.github.com>
2020-11-02 17:15:43 -0300

    Creacion funcion Suma

--- pruebaGit/main.c
+++ pruebaGit/main.c
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+int suma (int a, int b)
+{
+    return a+b;
+}
+
 int main()
 {
     printf("Hello world!\n");
     printf("Hello Rodri\n");
-    
+
     printf("Modificando desde github");
-	
-	printf("Probando el uso de ramas");
-	
-		printf("Desde rama 2");
-	
+
+	printf("Probando el uso de ramas desde GitBash\n");
+
+
+	printf("%d", suma(1,2));
+
+
     return 0;
 }
